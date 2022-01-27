99af2f49c6f05f3bfad4663c5ae1ba9cf7c8a1aa
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-01-27 19:21:22 +0900

    fix: disable react-query implicit refetch

--- src/components/root.tsx
+++ src/components/root.tsx
@@ -12,6 +12,8 @@ const queryClient = new QueryClient({
       retry: false,
       staleTime: 5 * 60 * 1000,
       cacheTime: 5 * 60 * 1000,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
     },
   },
 });
